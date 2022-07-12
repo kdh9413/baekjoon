d544a4c2a5b7f745724dda9fb366364ee61f08e3
kdh9413 <101865683+kdh9413@users.noreply.github.com>
2022-07-13 00:18:36 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,17 @@
+P = int(input())
+
+
+if P >= 90 and P <= 100 :
+    print('A')
+
+if P >= 80 and P <= 89 :
+    print('B')
+
+if P >= 70 and P <= 79 :
+    print('C')
+
+if P >= 60 and P <= 69 :
+    print('D')
+
+if P < 60:
+    print('F')
