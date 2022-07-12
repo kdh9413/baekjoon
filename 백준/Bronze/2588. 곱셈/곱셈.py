171f1b090d4aaa88666caf2c3f95e29a1d346662
kdh9413 <101865683+kdh9413@users.noreply.github.com>
2022-07-12 23:59:09 +0900

    [Bronze III] Title: 곱셈, Time: 64 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,19 @@
+a= input()
+b= input()
+A= int(a)
+
+d = list(b)
+
+g = int(d[0])
+h = int(d[1])
+i = int(d[2])
+
+w = A * i
+x = A * h
+y = A * g
+z = w + 10 * x + 100 * y
+
+print(w)
+print(x)
+print(y)
+print(z)
